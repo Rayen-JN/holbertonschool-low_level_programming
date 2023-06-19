253f2d2abdab7b8f53f3c4f72f41e16ccef6c723
rayen288700 <rayenchuk@gmail.com>
2023-06-19 08:39:31 -0700

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - print char lower and upper
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char ch = 'a';
+	char upper = 'A';
+		while (ch <= 'z')
+
+		{
+			putchar(ch);
+			ch++;
+		}
+		putchar('\n');
+		while (upper <= 'Z')
+
+		{
+			putchar(upper);
+			upper++;
+		}
+			putchar('\n');
+			return (0);
+
+}
