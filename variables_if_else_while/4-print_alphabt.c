49dd98fef769f4c735eb7a25afaf7855668e67f9
rayen288700 <rayenchuk@gmail.com>
2023-06-19 09:01:45 -0700

    4-print_alphabt.c fix

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print all char except q and e
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+
+	{
+		if (ch == 'e' || ch == 'q')
+			ch++;
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
