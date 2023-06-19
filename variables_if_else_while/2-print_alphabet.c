91466228b1a447b125fdfc445ca337931e6d90b0
rayen288700 <rayenchuk@gmail.com>
2023-06-19 08:24:50 -0700

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print one char
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch >= 'z')
+
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar("\n");
+	return (0);
+}
