b5f57fe46f64b025b0adb3e268e74f3fb2733a7c
rayen288700 <rayenchuk@gmail.com>
2023-06-19 10:10:09 -0700

    8-print_base16.c betty stayl fix

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16 in lowercase,
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 102)
+	{
+		if (num == 58)
+		{
+			num += 38;
+		}
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
