dcf15c974343f79032595b7c7c56a3e54b94aa15
rayen288700 <rayenchuk@gmail.com>
2023-06-20 10:22:41 -0700

    4-isalpha.c fix

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _islower - checks for lowercase character.
+ * _isalpha - checks for alpha character.
  * @c: is char
  * Return: 0 or 1
 */
-int _islower(int c)
+int _isalfa(int c)
 {
 	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'A')
 	{
