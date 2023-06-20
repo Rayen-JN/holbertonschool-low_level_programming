a0d4f89de9f82a76e8e91b1f72d1068d2a947e47
rayen288700 <rayenchuk@gmail.com>
2023-06-20 08:34:35 -0700

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main"
+#include <stdio.h>
+/**
+ * main - print all char
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+		_putchar(x);
+		x++;
+	return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef rayenheder_H
 #define rayenheder_H
 int _putchar(char c);
+void print_alphabet(void);
 #endif
 
