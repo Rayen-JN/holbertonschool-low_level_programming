6730c7080ad3ea69d4f29d684c5d76838e33cd62
rayen288700 <rayenchuk@gmail.com>
2023-06-20 09:45:12 -0700

    3-islower.c fix descreption

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower - checks for lowercase character.
+ *
+ * Return: 0 or 1
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (2);
+}
