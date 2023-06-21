e233b8113e5d2a20618c0a7ebad0344f0e200dd4
rayen288700 <rayenchuk@gmail.com>
2023-06-21 02:00:05 -0700

    6-abs.c i

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - computes the absolute value of an integer
+ * @x - this is variabel for the condichen
+ * Return: 0
+*/
+int _abs(int x)
+{
+	if (x >= 0)
+	{
+		return (x);
+	}
+	else
+		return (-x);
+}
