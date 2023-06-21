db46bc2a31870869e9c56a1b1d05e447aa9dfae9
rayen288700 <rayenchuk@gmail.com>
2023-06-21 13:37:00 -0700

    8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: 0
+*/
+void jack_bauer(void)
+{
+	int h = 0;
+
+	int m = 0;
+
+	while (h < 24)
+	{
+		while (m < 60)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+		}
+		m++;
+	}
+	h++;
+}
