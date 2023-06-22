df505ce4da1d8b3dea5c237feed229ba868b3ad0
rayen288700 <rayenchuk@gmail.com>
2023-06-22 09:34:07 -0700

    htray for 100 time

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,49 +1,24 @@
-#include"main.h"
+#include "main.h"
 
 /**
-*print_to_98 - fan
-*
-*@n: var
-*/
-
-void print_to_98(intn)
-
+ * print_to_98 - fan
+ *
+ * @n: vat
+ */
+void print_to_98(int n)
 {
-	inti , j;
-
-	if (n <= 98)
+	if (n >= 98)
 	{
-		for (i = n; i <= 98; i++)
-		{
-
-			if (i != 98)
-
-				printf("%d,", i);
-			else if (i == 98)
-				printf("%d\n", i);
-
-		}
-
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
 	}
 
-	else if (n >= 98)
-
+	else
 	{
-
-	for (j = n; j >= 98; j--)
-
-	{
-
-	if (j != 98)
-
-		printf("%d,", j);
-
-	else if (j == 98)
-
-		printf ("%d\n", j);
-
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
-
-	}
-
 }
+
