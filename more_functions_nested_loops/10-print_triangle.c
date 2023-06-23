d35051747680b8af4629d35f0666590a539685bc
rayen288700 <rayenchuk@gmail.com>
2023-06-23 15:23:49 -0700

    10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - This func
+ * @size: # number
+ *
+ * Return: 0
+ *
+ */
+
+void print_triangle(int size)
+{
+	int times, spaces, numerals;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+
+	for (times = 0; times < size; times++)
+	{
+		for (spaces = size - 1; spaces > times; spaces--)
+		{
+			_putchar(' ');
+		}
+		for (numerals = 0; numerals <= times; numerals++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
