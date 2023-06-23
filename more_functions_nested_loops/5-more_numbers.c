684c551591b136ed08d5feb8526d12882be881e1
rayen288700 <rayenchuk@gmail.com>
2023-06-23 12:06:52 -0700

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - This func
+ *
+ * Return:  0
+ */
+
+void more_numbers(void)
+{
+	int times, number;
+
+	for (times = 1; times <= 10; times++)
+	{
+		for (number = 0; number <= 14; number++)
+		{
+			if (number > 9)
+			{
+				putchar((number / 10) + '0');
+			}
+			putchar((number % 10) + '0');
+		}
+		putchar('\n');
+	}
+}
