d5c8cfd35a87f0324d7824fbd11bf6efd1d7de05
rayen288700 <rayenchuk@gmail.com>
2023-06-23 02:56:34 -0700

    1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - This function verify if the param is a digit or no.
+ * @c: Param to test.
+ *
+ * Return: Always 0 (If is Success).
+ *
+ * Author: Jeremias Erba.
+ */
+
+
+int _isdigit(int c)
+{
+return (c >= 48 && c <= 57);
+}
