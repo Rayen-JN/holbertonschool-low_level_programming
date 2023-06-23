b55edc31817bc07a33001cfa1c1fb1249ea4475e
rayen288700 <rayenchuk@gmail.com>
2023-06-23 12:24:34 -0700

    6-print_line.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - This func
+ * @n: re
+ *
+ * Return: 0
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int times, spaces;
+
+	if (n <= 0)
+	{
+		putchar('\n');
+		return;
+	}
+
+	for (times = 0; times < n; times++)
+	{
+		for (spaces = 0; spaces < times; spaces++)
+		{
+			putchar(' ');
+		}
+		putchar('\\');
+		putchar('\n');
+	}
+}
