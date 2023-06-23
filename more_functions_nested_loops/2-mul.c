f89315371727e3825147413c8d868c43df0cd7f0
rayen288700 <rayenchuk@gmail.com>
2023-06-23 03:10:52 -0700

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - This function multiply two numbers past for params.
+ * @a: First param.
+ * @b: Second param.
+ *
+ * Return: Always 0 (If is Success).
+ *
+ * Author: Jeremias Erba.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,5 +1,14 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+int mul(int a, int b);
+void print_numbers(void);
 int _isdigit(int c);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
 int _isupper(int c);
+void print_square(int size);
+void print_triangle(int size);
+
 #endif
