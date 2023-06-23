80f028002d49cc01eb947923d0b3f3ff4ed180ce
rayen288700 <rayenchuk@gmail.com>
2023-06-23 02:43:02 -0700

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - This function test if is Lower or Upper.
+ * @c: Param to test.
+ *
+ * Return: Always 0 (If is Success).
+ *
+ * Author: Jeremias Erba.
+ */
+
+int _isupper(int c)
+{
+    return (c >= 'A' && c <= 'Z');
+}
