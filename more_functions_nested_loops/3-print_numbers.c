1bdd61d1650235fbd42eb10912f2c49571a0f26b
rayen288700 <rayenchuk@gmail.com>
2023-06-23 04:27:58 -0700

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - This function print 0-9 numbers.
+ *
+ * Return: Always 0 (If is Success).
+ *
+ * Author: Jeremias Erba.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+}
