dddc67c6be05754fd4f019a01c10e403eefc1544
rayen288700 <rayenchuk@gmail.com>
2023-06-23 12:45:09 -0700

    9

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - funcc
+ * @size: num
+ *
+ * Return: 0
+ *
+ */
+
+void print_square(int size)
+{
+	int times, simbol;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+		return;
+	}
+
+	for (times = 0; times < size; times++)
+	{
+		for (simbol = 0; simbol < size; simbol++)
+		{
+			putchar('#');
+		}
+		putchar('\n');
+	}
+}
