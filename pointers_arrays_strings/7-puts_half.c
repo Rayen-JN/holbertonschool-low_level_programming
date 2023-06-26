f397437b2da9067cf38f70b2cae3bb5424d9710c
rayen288700 <rayenchuk@gmail.com>
2023-06-26 09:44:53 -0700

    try

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -19,9 +19,9 @@ void puts_half(char *str)
 		}
 	}
 	else
-		for (n = len / 2; n <= (len - 1); n++)
+		for (n = (len / 2) + 1; n <= (len - 1); n++)
                 {
-                        _putchar(str[n + 1]);
+                        _putchar(str[n]);
                 }
 
 	_putchar('\n');
