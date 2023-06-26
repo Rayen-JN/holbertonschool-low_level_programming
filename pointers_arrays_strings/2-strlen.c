58e2a2fe0af860168d5f9579aca0aaeff1039350
rayen288700 <rayenchuk@gmail.com>
2023-06-26 01:56:03 -0700

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strlen - len of str
+* @s: the str
+* Return: int str
+*/
+
+int _strlen(char *s)
+{
+	int x = 0;
+
+	for (; s[x]; x++)
+	{
+	return (x);
+	}
+}
