cb1705999243bdd61dffcb35b8b09bd7029835b5
rayen288700 <rayenchuk@gmail.com>
2023-06-26 00:59:38 -0700

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* reset_to_98 - func change valu
+* @n: value
+* Return: 0
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98 ;
+
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef HEADER_FILE_NAME
+#define HEADER_FILE_NAME
+
+void reset_to_98(int *n);
+int _putchar(char c);
+
+#endif
