d8e8c3d9ce164b1d8fd904e93382f5dc8d8cb4c4
rayen288700 <rayenchuk@gmail.com>
2023-06-28 22:20:09 -0700

    100 fix

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _ato - convert str to int
+* @s: arry
+* Return: int
+*
+*/
+int _atoi(char *s)
+{
+unsigned int number = 0, sign = 1;
+
+	while (*s)
+	{
+		if (*s >= '0' && *s <= '9')
+			number = (number * 10) + (*s - '0');
+		else if (*s == 45)
+			sign = sign * -1;
+		else if (number > 0)
+			break;
+		
+	}
+	return (number );
+
+}
