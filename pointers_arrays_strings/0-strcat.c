ed6e209dfd73a585cd31aa0b9ce6b82609e4fd21
rayen288700 <rayenchuk@gmail.com>
2023-06-28 23:07:29 -0700

    0-strcat.c

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* _strcat - add to str
+*
+* @dest: destination
+* @src: source
+* Return: str
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int c1 = 0;
+	int c2 = 0;
+
+	while (dest[c2])
+	{
+		c2++;
+	}
+
+	while (src[c1])
+	{
+		dest[c2++] = src[c1];
+	}
+
+}
