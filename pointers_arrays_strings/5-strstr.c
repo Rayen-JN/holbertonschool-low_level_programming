46830f46b5522c94569c4d36a9842896516c19fc
rayen288700 <rayenchuk@gmail.com>
2023-06-30 14:36:35 -0700

    5-fic

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strstr - func
+ *
+ * Return: numb
+ * @haystack: Str
+ * @needle: arry 
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int slots;
+
+	if (*needle == 0)
+	{
+		return (haystack);
+	}
+
+	while (*haystack)
+	{
+		slots = 0;
+
+		if (haystack[slots] == needle[slots])
+		{
+			while (haystack[slots] == needle[slots])
+			{
+				if (needle[slots + 1] == '\0')
+				{
+					return (haystack);
+				}
+				slots++;
+			}
+		}
+		haystack++;
+	}
+	return (0);
+}
