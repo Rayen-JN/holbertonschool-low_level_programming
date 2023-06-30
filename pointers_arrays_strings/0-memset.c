820f8227a0b5bcd0b34484086f4f0ea65ef0c3af
rayen288700 <rayenchuk@gmail.com>
2023-06-30 13:45:58 -0700

    memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - func
+ *
+ * @s: char
+ * @b: con
+ * @n: Num
+ * Return: sr
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -21,5 +21,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *_memset(char *s, char b, unsigned int n);
 
 #endif
