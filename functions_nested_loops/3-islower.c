7e5fbd0f64b88298aaa5d3d66b6d43ec86d4497f
rayen288700 <rayenchuk@gmail.com>
2023-06-30 05:01:40 -0700

    try to commit

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- * _islower - checks for lowercase character.
- * @c: is char
- * Return: 0 or 1
-*/
-int _islower(int c)
+ * print_numbers - This func
+ *
+ * Return: 0
+ *
+ */
+void print_numbers(void)
 {
-	if (c >= 'a' && c <= 'z')
+	int i;
+
+	for (i = 0; i < 10; i++)
 	{
-		return (1);
+		putchar(i + '0');
 	}
-	else
-	{
-		return (0);
-	}
+	putchar('\n');
 }
