a803115bd7a26069a7bf759438f01bdd29179e98
rayen288700 <rayenchuk@gmail.com>
2023-06-30 00:21:32 -0700

    2 compar str

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -16,5 +16,6 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 
 #endif
