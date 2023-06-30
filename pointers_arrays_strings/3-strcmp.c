33a1a41aa82c24b2ffd675fc376d4e76288dfcc6
rayen288700 <rayenchuk@gmail.com>
2023-06-30 00:20:02 -0700

    2 compar str

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncpy - fun
+ * @dest: destination
+ * @src: sourse
+ * @n: num
+ *
+ *Return: str
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i  < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
