2eea128aac2c1d164b00848cb8b7bfa2f828aff3
rayen288700 <rayenchuk@gmail.com>
2023-06-30 13:00:16 -0700

    leet

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * leet - func
+ *
+ * @n: arry
+ * Return: char
+ */
+
+char *leet(char *n)
+{
+	int count = 0;
+	int count2 = 0;
+	char letters[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L', '\0'};
+	char numbers[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	for (; n[count]; count++)
+	{
+		for (count2 = 0; letters[count2]; count2++)
+		{
+			if (n[count] == letters[count2])
+			{
+				n[count] = numbers[count2];
+			}
+		}
+	}
+	return (n);
+
