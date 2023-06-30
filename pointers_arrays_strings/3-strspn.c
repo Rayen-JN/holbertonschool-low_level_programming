ca2ff2225b60f6feb5f4246cab9f51fad9b6311c
rayen288700 <rayenchuk@gmail.com>
2023-06-30 14:16:47 -0700

    this is to fix

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,35 +10,35 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-    int c, m;
-    unsigned int count = 0;
+	int c, m;
+	unsigned int count = 0;
 
-    for (c = 0; s[c] != '\0'; c++)
-    {
-        if (s[c] != ' ')
-        {
-            int found = 0;
+	for (c = 0; s[c] != '\0'; c++)
+	{
+	if (s[c] != ' ')
+	{
+	int found = 0;
 
-            for (m = 0; accept[m] != '\0'; m++)
-            {
-                if (s[c] == accept[m])
-                {
-                    found = 1;
-                    break;
-                }
-            }
+	for (m = 0; accept[m] != '\0'; m++)
+	{
+		if (s[c] == accept[m])
+		{
+			found = 1;
+			break;
+		}
+	}
 
-            if (!found)
-                return count;
+		if (!found)
+		return (count);
 
-            count++;
-        }
-        else
-        {
-            return count;
-        }
-    }
+	count++;
+	}
+	else
+	{
+	return (count);
+	}
+	}
 
-    return count;
+	return (count);
 }
 
