2ec905ecdd3ab1d6f9add64ca3e16d664c46dbfc
rayen288700 <rayenchuk@gmail.com>
2023-07-04 01:20:01 -0700

    recurjen power

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* _pow_recursion - power func
+*
+* @x: numb
+* @y: power
+* Return: int
+*/
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+else if (y == 1)
+return (x);
+else  if (y == 0)
+return (1)
+else
+{
+return (x * _pow_recursion(x, y - 1 ));
+}
+}
