76bf2b32b001559fc144159b83b98b50fe28dcf0
rayen288700 <rayenchuk@gmail.com>
2023-07-05 02:50:29 -0700

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - func
+ * @argc: num
+ * @argv: array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
