2b966087b60f9b1cd14f11a73663585d05c049e7
rayen288700 <rayenchuk@gmail.com>
2023-07-05 02:51:25 -0700

    2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - func
+ * @argc: num
+ * @argv: array
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
