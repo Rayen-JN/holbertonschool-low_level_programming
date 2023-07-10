6a3bed706058c24f2d548a5ffa94e951b67915ae
rayen288700 <rayenchuk@gmail.com>
2023-07-10 14:14:00 +0100

    fix betty

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -10,12 +10,14 @@
 char *_strdup(char *str)
 {
 char *ptr;
-int len = strlen(str);
+int len;
 int i;
 if (str == NULL)
 {
 	return (NULL);
 }
+else{
+len = strlen(str);
 ptr = (char *)malloc((len + 1) * sizeof(char));
 if (ptr == NULL)
 {
@@ -30,5 +32,6 @@ for (i = 0; i < len; i++)
 ptr[i] = '\0';
 return (ptr);
 free(ptr);
+}
 }
 }
