044aa787c2f3b7caa48363febcff2556f806b508
rayen288700 <rayenchuk@gmail.com>
2023-07-10 15:22:13 +0100

    concatinate strs

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,11 +16,15 @@ char *str_concat(char *s1, char *s2)
 	int len1;
 	int len2;
 	int i;
-	if(s1 == NULL || s2 == NULL)
+	if(s1 == NULL && s2 == NULL)
 	{
 		return ('\0');
 	}
-	else if(s1 == NULL && s2 == NULL)
+	else if(s1 == NULL)
+	{
+		return ('\0');
+	}
+	else if (s2 == NULL)
 	{
 		return ('\0');
 	}
