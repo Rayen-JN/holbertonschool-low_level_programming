8e09b11d6909081bf35093e63cb56b4580d23a59
rayen288700 <rayenchuk@gmail.com>
2023-07-10 12:30:07 +0100

    create arry

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ * create_array - func
+ * @size: size of arry
+ * @c: the char
+ *
+ */
+char *create_array(unsigned int size, char c)
+{
+    int i;
+    int *ptr;
+
+    ptr = (char *)malloc(size * sizeof(char));
+    if (size == 0 || ptr == NULL)
+    {
+        return NULL;
+    }
+    else
+        for (i = 0; i < size; i++)
+            *ptr[i] = c;
+}
