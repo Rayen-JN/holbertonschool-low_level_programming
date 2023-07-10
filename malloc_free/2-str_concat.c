0ccfb28db97ce99b6424b800b8d914c99bfb7be8
rayen288700 <rayenchuk@gmail.com>
2023-07-10 15:55:57 +0100

    concatinate strs

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,10 +17,10 @@ char *str_concat(char *s1, char *s2)
 	int len2;
 	int i;
 	if(s1 == NULL)
-	{	s1 = '\0';
+	{	s1 = "\0";
 	}
 	if(s2 == NULL)
-	{	s2 = '\0';
+	{	s2 = "\0";
 	}
 		len1 = strlen(s1);
 		len2 = strlen(s2);
