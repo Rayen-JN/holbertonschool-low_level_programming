7a185afe5db5b88ded69997cb1a1ee1f31facaaf
rayen288700 <rayenchuk@gmail.com>
2023-07-12 12:35:16 +0100

    free the 2d arry

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include <stdlib>
+#include "main.h"
+#include <stdio.h>
+/**
+ * free_grid - this is function for free 2d  array
+ * @height: the he
+ */
+void free_grid(int **grid, int height)
+{
+	for (; i >= 0; i--)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
