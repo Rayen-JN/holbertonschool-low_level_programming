9a6bd94c0dc8c9bc8928f9ef38461fc3e85dff44
rayen288700 <rayenchuk@gmail.com>
2023-07-13 11:56:29 +0100

    allocate memory using malloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -16,11 +16,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	ptr = malloc(nmemb * sizeof(int));
-	if (ptr == NULL)
+	if (ptr == 0)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < (nmemb * size); i++)
+	for (i = 0; i < nmemb; i++)
 	{
 		ptr[i] = 0;
 	}
