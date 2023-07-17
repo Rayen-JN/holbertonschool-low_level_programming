036401621189e64dac9eabc7718832324c8c974a
rayen288700 <rayenchuk@gmail.com>
2023-07-17 17:57:18 +0100

    HEADER_FILE

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 /**
  * struct dog - struct information
@@ -20,5 +21,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
