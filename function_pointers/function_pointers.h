b09b037a7a0da8f9868fe7e702d99d351baf288c
rayen288700 <rayenchuk@gmail.com>
2023-07-19 13:23:30 +0100

    HEDER FILE

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,10 +1,7 @@
 #ifndef HEDER
 #define HEDER
 
-//-------------------------//
 #include <stdio.h>
-//-------------------------//
-
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
