44647dec8415d532c47bf535687b5d62200f473f
rayen288700 <rayenchuk@gmail.com>
2023-07-21 19:02:11 +0100

    THE OP FUNCTION

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/**
+ * op_add - number
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - def numb
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: the def number
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - return number.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - div
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: /
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - op mode 
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: %
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
