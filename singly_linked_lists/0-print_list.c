f8e32da2b0eb9c670e0510f36cdb82ab01445996
rayen288700 <rayenchuk@gmail.com>
2023-08-03 16:57:14 +0100

    PRINT VALUE

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * print_list - print val node
+ * @h: pointer the a node
+ *
+ * Return: number of node
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
