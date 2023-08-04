08122ba96b38f7e391092590d63f6931266fa6eb
rayen288700 <rayenchuk@gmail.com>
2023-08-04 12:19:31 +0100

    fix cod

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->next = *head;
 	if (*head != NULL)
 	{
-		*head->prev = new;
+		(*head)->prev = new;
 	}
 	*head = new;
 	return (new);
