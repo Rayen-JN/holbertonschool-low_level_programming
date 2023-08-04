06872090da9443062cc3d11a7304297e0bdda113
rayen288700 <rayenchuk@gmail.com>
2023-08-04 10:40:50 +0100

    print func

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_dlistint -  prints
+ * @h:pointer
+ * Return:the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *ptr;
+	int nb;
+
+	ptr = h;
+	nb = 0;
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		ptr = ptr->next;
+	
+	}
+	return (nb);
+}
