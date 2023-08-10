02d985900a7218112512bb930a5ed7046b859d35
rayen288700 <rayenchuk@gmail.com>
2023-08-10 10:55:12 +0100

    fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,10 +13,10 @@ int create_file(const char *filename, char *text_content)
 	int len = strlen(text_content);
 
 	if (filename == NULL)
-	return (-1)
+	return (-1);
 	if (text_content == NULL)
 	return (-1);
-	fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY,0600);
 	if (fd == -1)
 		return (0);
 	w = write (fd,text_content,len)
