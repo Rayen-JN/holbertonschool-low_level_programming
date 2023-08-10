fd2896a8222f9c9084d006f3f7ca2c5bec1edd27
rayen288700 <rayenchuk@gmail.com>
2023-08-10 11:07:20 +0100

    fix style

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,19 +13,18 @@ int create_file(const char *filename, char *text_content)
 	int len = 0;
 
 	if (filename == NULL)
-	return (-1);
+		return (-1);
 	if (text_content)
 	{
 		len = strlen(text_content);
 	}
 
-
-	fd = open(filename, O_WRONLY|O_CREAT|O_TRUNC,0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	w = write (fd,text_content,len);
+	w = write(fd, text_content, len);
 	if (w == -1)
-	return (-1);
+		return (-1);
 	close(fd);
 	return (1);
 }
