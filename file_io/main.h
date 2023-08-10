3313923b5e8a0c6629b0c636ea52c107cb2f44bf
rayen288700 <rayenchuk@gmail.com>
2023-08-10 10:59:22 +0100

    include stddef.h

--- file_io/main.h
+++ file_io/main.h
@@ -8,6 +8,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stddef.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
