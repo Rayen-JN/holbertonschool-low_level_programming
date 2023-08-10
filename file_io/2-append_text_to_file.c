b7bd79bcdaf42217b37effa2022d394f4806e1c0
rayen288700 <rayenchuk@gmail.com>
2023-08-10 12:16:19 +0100

    fix

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		len = strlen(text_content);
 	fd = open(filename, O_WRONLY);
 	w = write(fd, text_content, len);
-	if (fd == NUll || w == NULL)
+	if (fd == NULL || w == NULL)
 		return (-1);
 
 	close(fd);
