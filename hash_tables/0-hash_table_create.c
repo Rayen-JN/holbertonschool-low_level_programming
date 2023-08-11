cb4fae956a409f7d99dc170da16a2a43799339f6
rayen288700 <rayenchuk@gmail.com>
2023-08-11 09:59:56 +0100

    fix code

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -19,11 +19,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
-		return (NULL);
-
-	for (i = 0; i < size; i++)
 	{
-		ht->array[i] = NULL;
+		free(ht);
+		return (NULL);
 	}
-	return (ht);
-}
+
+		for (i = 0; i < size; i++)
+		{
+			ht->array[i] = NULL;
+		}
+		return (ht);
+	}
