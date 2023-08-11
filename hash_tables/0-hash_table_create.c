c3408de17efc43234e6761900757121541af6c10
rayen288700 <rayenchuk@gmail.com>
2023-08-11 09:52:48 +0100

    fix code

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -17,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (-1);
 
 	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t) * size);
+	ht->array = malloc(sizeof(hash_node_t*) * size);
 	if (ht->array == NUll)
 		return (-1);
 
