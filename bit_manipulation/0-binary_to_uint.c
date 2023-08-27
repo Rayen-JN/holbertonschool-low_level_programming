0917220655eb50aecebac62042c3d6e46f629fc5
rayen288700 <rayenchuk@gmail.com>
2023-08-27 12:09:59 +0100

    converts a binary to number

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number
+ * @b: string binary number
+ *
+ * Return: number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int val = 0;
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		val = 2 * val + (b[i] - '0');
+	}
+
+	return (val);
+}
