ae55660ce761984b4ffce009e824e3d54df8ee10
rayen288700 <rayenchuk@gmail.com>
2023-08-28 13:28:36 +0100

    fix betty line

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,20 +13,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *value_copy;
 	unsigned long int index, i;
 
-	if (key == NULL)
-	{
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-	}
-	if (*key == '\0')
-	{
-		return (0);
-	}
-	if (value == NULL)
-	{
-		return (0);
-	}
-	
-	
 
 	value_copy = strdup(value);
 	if (value_copy == NULL)
